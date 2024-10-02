dc5c04ff4055c964164adbcba86436c2a77fdba0
Niklas Vesper <132565620+MrTheLukasBoom@users.noreply.github.com>
2024-10-02 15:28:44 +0200

    Update Neighborhood testing.py

--- scripts/Xin et al./Neighborhood testing.py
+++ scripts/Xin et al./Neighborhood testing.py
@@ -1,8 +1,4 @@
-import random
-import scandir as sd
-import os
 import pandas as pd
-import re
 from scipy.stats import mannwhitneyu
 
 folder_dir_high = r'F:\ATF6 FAK\All\High\CD8\CD8_immune_neighborhood.csv'
@@ -28,49 +24,3 @@ for type in types:
         print('found one')
 
     print('------------')
-
-#Filedirlist = []
-
-
-'''
-for paths, dirs, files in sd.walk(folder_dir): #goes throw all files and folders in given directory
-
-
-    for file in os.listdir(paths): #goes throw all files in a folder
-        filedir = os.path.join(paths, file) #returns fuull file directory
-
-        if filedir.endswith("CD8_neighborhood.csv"):
-
-
-            filename = os.path.basename(file)
-            filename_string = str(filename)
-            filedir_string = str(filedir)[:-len(filename)]
-
-            if 'Tumor_1' in filename_string:
-                Filedirlist.append(filedir)
-
-pdlist = []
-
-for el in Filedirlist:
-    pdlist. append(pd.read_csv(el))
-
-Big_data = pd.concat([t for t in pdlist], ignore_index=True)
-
-Big_interaction = []
-Total_Cell_count = []
-
-for index, cell in Big_data.iterrows():
-
-    if cell['Cell_types'] == "['Immune cell', 'CD8 T cell']":
-        Total_Cell_count.append('+1')
-
-        Neigboorhood = re.split(r'], ', cell['CD8 neigboors'])
-
-        Cell_list = []
-        for x in Neigboorhood:
-            Cell_list.append(x.replace('[', '').replace(']', ''))
-            Big_interaction.append(x.replace('[', '').replace(']', ''))
-
-print(len(Total_Cell_count))
-print(Big_interaction)
-'''
