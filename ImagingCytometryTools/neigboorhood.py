ae4b45fcae8082df9670c48f5d8607a5cfd67fba
Niklas Vesper <132565620+MrTheLukasBoom@users.noreply.github.com>
2024-10-02 11:32:35 +0200

    Update neigboorhood.py

--- ImagingCytometryTools/neigboorhood.py
+++ ImagingCytometryTools/neigboorhood.py
@@ -54,3 +54,36 @@ def neigboorhood(Cells):
     Cells['Cell_number'] = Cell_number
 
     return(Cells)
+
+
+# gives you the neigbooring cell types
+def neigboorhood_cell_type(Cell_type, Subtype,file):
+
+    Neigboorhood_fin= []
+    for index, cell in file.iterrows():
+
+        if cell['Cell_types'] == Cell_type and cell['immune_type'] == Subtype:
+
+            Neigboorhood = re.split(r',', cell['Neigboorhood'].replace('[', '').replace(']', ''))
+
+            Neigboorhood_list = []
+            for x in Neigboorhood:
+                try:
+                    Neigboorhood_list.append(int(float(x)))
+                except ValueError:
+                    pass
+
+
+            Neigboorhood_cell = []
+            for index, cell in file.iterrows():
+
+                if cell['Cell_number'] in Neigboorhood_list:
+                    Neigboorhood_cell.append(cell['Cell_types'])
+
+            Neigboorhood_fin.append(Neigboorhood_cell)
+            print(Neigboorhood_cell)
+
+        else:
+            Neigboorhood_fin.append('currently not of interest')
+
+    return(Neigboorhood_fin)
