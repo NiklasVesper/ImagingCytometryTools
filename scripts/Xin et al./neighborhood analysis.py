c6b83323443981c1446e934ce249d5ff15272125
Niklas Vesper <132565620+MrTheLukasBoom@users.noreply.github.com>
2024-10-15 17:00:41 +0200

    Update neighborhood analysis.py

--- scripts/Xin et al./neighborhood analysis.py
+++ scripts/Xin et al./neighborhood analysis.py
@@ -5,82 +5,86 @@ import scandir as sd
 import numpy as np
 import shapely.geometry
 import warnings
-import re
 
-''''''
+'''
+The "neighborhood analysis" script examines the spatial proximity of all cells within the tissue.
+The script takes files that were previously generated with the "match cells with their nuclei" script.
+'''
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def neigboorhood(Cells):
+#spatial proximity of all cells within the tissue
+def neighborhood(Cells):
     print('I am looking for my friends :)')
 
-    # create unique list of names of images
-    UniqueNames_Full_cell = Cells.ImageNumber.unique()  # creates a list of all unique images
+    #creates lists of all unique images
+    UniqueNames_Full_cell = Cells.ImageNumber.unique()
 
-    # create a data frame dictionary to store your data frames
+    #creates a pandas data frame dictionary of all unique images
     DataFrameDict_Full_cell = {elem: pd.DataFrame() for elem in UniqueNames_Full_cell}  # creates a dictionary of all unique images
 
-    # Resets indexes for conected images and they identefied objects
+    #resets indexes for identified objects in the image blocks
     for key in DataFrameDict_Full_cell.keys():
         DataFrameDict_Full_cell[key] = Cells[:][Cells.ImageNumber == key].reset_index()
 
     Cell_number = []
-    Neigboorhood = []
+    Neighborhood = []
 
-    # Takes connectet images and they identefied objects form from the Dictionary
+    #takes connected images and they objects form from the dictionary
     for key, value in DataFrameDict_Full_cell.items():
 
+        #gets all x and y positions of cells on an image and creates an array of them
         x_cell_t = DataFrameDict_Full_cell[key]['Location_Center_X'].to_list()
         y_cell_t = DataFrameDict_Full_cell[key]['Location_Center_Y'].to_list()
         position_cells = np.array(list(zip(x_cell_t, y_cell_t)))
 
-        count_cell = -1
-        for cell in position_cells:
-            cell_position = shapely.geometry.Point(cell)
-            neighborhood_radius = DataFrameDict_Full_cell[key]['AreaShape_MaxFeretDiameter'].mean()
-            cell_neighborhood = shapely.geometry.Point(cell).buffer(neighborhood_radius * 1.5)
-            count_cell = count_cell + 1
+        count_cell = -1 #counter
+        for cell in position_cells: #goes throw all cell positions
+            cell_position = shapely.geometry.Point(cell) #creates a point at the center of the cell
+            neighborhood_radius = DataFrameDict_Full_cell['AreaShape_MaxFeretDiameter'].mean() #gives you the mean MaxFeretDiameter of all cells
+            cell_neighborhood = shapely.geometry.Point(cell).buffer(neighborhood_radius * 1.5) #defines the radius for the spatial proximity
+            count_cell = count_cell + 1 #ticks up the counter
             Cell_number.append(count_cell)
 
-            Whats_in_the_hood = []
-            count_other_cell = -1
-            for other_cell in position_cells:
-                other_cell_position = shapely.geometry.Point(other_cell)
-                count_other_cell = count_other_cell + 1
+            Whats_in_the_hood = [] #list for the individual neighborhood of a cell
+            count_other_cell = -1 #counter
+            for other_cell in position_cells: #goes throw all other cell positions
+                other_cell_position = shapely.geometry.Point(other_cell) #creates a point at the center of the cell
+                count_other_cell = count_other_cell + 1 #ticks up the counter
 
-                if other_cell_position == cell_position:
+                if other_cell_position == cell_position: #skipps the own cell position
                     continue
-                elif other_cell_position.within(cell_neighborhood) == True:
+                elif other_cell_position.within(cell_neighborhood) == True: #if a cell is within the defined radius it will be considered to be within the neighborhood
                     Whats_in_the_hood.append(count_other_cell)
 
-            Neigboorhood.append(Whats_in_the_hood)
+            Neighborhood.append(Whats_in_the_hood)
 
-    Cells['Neigboorhood'] = Neigboorhood
-    Cells['Cell_number'] = Cell_number
+    Cells['Neighborhood'] = Neighborhood #creates a new columb for the neighborhood
+    Cells['Cell_number'] = Cell_number #creates a new columb for the cell number
 
     return (Cells)
 
-folder_dir = r'D:\ATF6'  # folder directory
 
-# first neighborhood analysis
-for paths, dirs, files in sd.walk(folder_dir):  # goes throw all files and folders in given directory
+folder_dir = r'D:\ATF6'  #folder directory
 
-    for file in os.listdir(paths):  # goes throw all files in a folder
-        filedir = os.path.join(paths, file)  # returns fuull file directory
+for paths, dirs, files in sd.walk(folder_dir):  #goes throw all files and folders in given directory
 
-        if filedir.endswith("single_cells_and_organells.csv"):  # returns all files that end with txt
+    for file in os.listdir(paths):  #goes throw all files in a folder
+        filedir = os.path.join(paths, file)  #returns full file directory
 
-            filename = os.path.basename(file)  # gives you the file name
-            filename_string = str(filename)
+        if filedir.endswith("single_cells_and_organells.csv"):  #checks if the file has 'single_cells_and_organells.csv' in its name
 
-            filedir_string = str(filedir)[:-len(filename) - 5]  # creates a file directory string
+            filename = os.path.basename(file) #gives you the file name
+            filename_string = str(filename) #turns the filename into a string
 
-            filedir_images_string = filedir_string + r'\neigboorhood'  # creates a new directory
+            filedir_string = str(filedir)[:-len(filename) - 5]  #gives you the file directory as a string
 
-            if os.path.isdir(filedir_images_string) == True:  # checks if the new directory allready exists
+            filedir_images_string = filedir_string + r'\neigboorhood'  #creates a new directory
+
+            if os.path.isdir(filedir_images_string) == True: #checks if the new directory allready exist
                 pass
-            else:
-                os.makedirs(filedir_images_string)  # creates the new directory
+            else: #creates a new directory if necessary
+                os.makedirs(filedir_images_string)
 
-            neigboorhood_all = neigboorhood(pd.read_csv(filedir))  # calculates the neighborhood
-            neigboorhood_all.to_csv(filedir_images_string + '/' + filename_string[:-18] + '_neighborhood.csv')  # exports the analysis
+            neigboorhood_all = neighborhood(pd.read_csv(filedir))  #calculates the spatial proximity of all cells within the tissue
+            neigboorhood_all.to_csv(filedir_images_string + '/' + filename_string[:-18] + '_neighborhood.csv')  #exports the file
