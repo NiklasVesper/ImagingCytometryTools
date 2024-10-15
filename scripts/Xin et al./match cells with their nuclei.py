c24233c0e23c537c3340d8eea48f8393df4d1898
Niklas Vesper <132565620+MrTheLukasBoom@users.noreply.github.com>
2024-10-15 15:33:09 +0200

    Update match cells with their nuclei.py

--- scripts/Xin et al./match cells with their nuclei.py
+++ scripts/Xin et al./match cells with their nuclei.py
@@ -36,7 +36,7 @@ def get_markers_from_segmentation(Cells):
             marker_str = str(marker)  #turns marker into strings for the list
             clean_markers.append(marker_str)  #append to marker list
 
-    return(clean_markers)  #returns marker list
+    return (clean_markers)  #returns marker list
 
 
 #match cells with their nuclei for mostly round cells
@@ -53,16 +53,16 @@ def cell_to_organell_basic(Cells, Cytoplasm, Nucleus):
     DataFrameDict_Cytoplasm = {elem: pd.DataFrame() for elem in UniqueNames_Cytoplasm}
     DataFrameDict_Nucleus = {elem: pd.DataFrame() for elem in UniqueNames_Nucleus}
 
-    #creates column name list for  to store the relevant information of matched subcellular locations
+    #creates column name list for the pandas data frame that stores the relevant information of matched subcellular locations
     columns = ['ImageNumber', 'Location_Center_X', 'Location_Center_Y', 'AreaShape_MinFeretDiameter', 'AreaShape_MaxFeretDiameter', ]
     for markers in get_markers_from_segmentation(Cells):
         columns.append(markers + '_Cell')
         columns.append(markers + '_Cytoplasm')
         columns.append(markers + '_Nucleus')
 
-    single_nuclear_cells = pd.DataFrame(columns=columns)  # empty dataframe for all markers and subcellular locations
+    single_nuclear_cells = pd.DataFrame(columns=columns)  #empty pandas data frame that stores the relevant information of matched subcellular locations
 
-    # Resets indexes for image blocks and they identefied objects
+    #resets indexes for identified objects in the image blocks
     for key in DataFrameDict_Full_cell.keys():
         DataFrameDict_Full_cell[key] = Cells[:][Cells.ImageNumber == key].reset_index()
 
@@ -72,46 +72,49 @@ def cell_to_organell_basic(Cells, Cytoplasm, Nucleus):
     for key in DataFrameDict_Nucleus.keys():
         DataFrameDict_Nucleus[key] = Nucleus[:][Nucleus.ImageNumber == key].reset_index()
 
-    # Takes connectet images and they identefied objects form from the Dictionary
+    #takes connectet images and they identefied objects form from the Dictionary
     for key, value in DataFrameDict_Full_cell.items():
 
-        x_cell_t = DataFrameDict_Full_cell[key]['Location_Center_X'].to_list()  # gets all x positions of cells on a image
-        y_cell_t = DataFrameDict_Full_cell[key]['Location_Center_Y'].to_list()  # gets all y positions of cells on a image
-        position_cells = np.array(list(zip(x_cell_t, y_cell_t)))  # creates an array of all the x and y positions of the cells
+        #gets all x and y positions of cells on an image and creates an array of them
+        x_cell_t = DataFrameDict_Full_cell[key]['Location_Center_X'].to_list()
+        y_cell_t = DataFrameDict_Full_cell[key]['Location_Center_Y'].to_list()
+        position_cells = np.array(list(zip(x_cell_t, y_cell_t)))
 
-        x_cytoplasm_t = DataFrameDict_Cytoplasm[key]['Location_Center_X'].to_list()  # gets all x positions of cytoplasms on a image
-        y_cytoplasm_t = DataFrameDict_Cytoplasm[key]['Location_Center_Y'].to_list()  # gets all y positions of cytoplasms on a image
-        position_cytoplasm = np.array(list(zip(x_cytoplasm_t, y_cytoplasm_t)))  # creates an array of all the x and y positions of the cytoplasms
+        #gets all x and y positions of cytoplasm on an image and creates an array of them
+        x_cytoplasm_t = DataFrameDict_Cytoplasm[key]['Location_Center_X'].to_list()
+        y_cytoplasm_t = DataFrameDict_Cytoplasm[key]['Location_Center_Y'].to_list()
+        position_cytoplasm = np.array(list(zip(x_cytoplasm_t, y_cytoplasm_t)))
 
-        x_nucleus_t = DataFrameDict_Nucleus[key]['Location_Center_X'].to_list()  # gets all x positions of nuclei on a image
-        y_nucleus_t = DataFrameDict_Nucleus[key]['Location_Center_Y'].to_list()  # gets all y positions of nuclei on a image
-        position_nucleus = np.array(list(zip(x_nucleus_t, y_nucleus_t)))  # creates an array of all the x and y positions of the nuclei
+        #gets all x and y positions of nuclei on an image and creates an array of them
+        x_nucleus_t = DataFrameDict_Nucleus[key]['Location_Center_X'].to_list()
+        y_nucleus_t = DataFrameDict_Nucleus[key]['Location_Center_Y'].to_list()
+        position_nucleus = np.array(list(zip(x_nucleus_t, y_nucleus_t)))
 
-        count_cell = -1  # counter
-        for cell in position_cells:  # goes throw all cell positions
-            cell_diameter = DataFrameDict_Full_cell[key]['AreaShape_MinFeretDiameter'].to_list()[count_cell]  # creates the smallest possible cell diameter
-            cell_shape = shapely.geometry.Point(cell).buffer(cell_diameter / 2)  # creates a circle representing the cell area
-            count_cell = count_cell + 1  # adds a counter to the counter
+        count_cell = -1  #counter
+        for cell in position_cells:  #goes throw all cell positions
+            cell_diameter = DataFrameDict_Full_cell[key]['AreaShape_MinFeretDiameter'].to_list()[count_cell]  #creates the smallest possible cell diameter
+            cell_shape = shapely.geometry.Point(cell).buffer(cell_diameter / 2)  #creates a circle representing the cell area
+            count_cell = count_cell + 1  #ticks up the counter
 
-            count_cytoplasm = -1  # counter
-            for cytoplasm in position_cytoplasm:  # goes throw all cytoplasm positions
-                cytoplasm_position = shapely.geometry.Point(cytoplasm)  # creates a point at the center of the cytoplasm object
-                count_cytoplasm = count_cytoplasm + 1  # adds a counter to the counter
+            count_cytoplasm = -1  #counter
+            for cytoplasm in position_cytoplasm:  #goes throw all cytoplasm positions
+                cytoplasm_position = shapely.geometry.Point(cytoplasm)  #creates a point at the center of the cytoplasm object
+                count_cytoplasm = count_cytoplasm + 1  #ticks up the counter
 
-                if cytoplasm_position.within(cell_shape) == True:  # checks if the center of the cytoplasm is within the cell
+                if cytoplasm_position.within(cell_shape) == True:  #checks if the center of the cytoplasm is within the cell
 
-                    count_nucleus = -1  # counter
-                    nucleus_list = []  # list of all found nuclei
-                    for nucleus in position_nucleus:  # goes throw all nuclei positions
-                        nucleus_position = shapely.geometry.Point(nucleus)
-                        count_nucleus = count_nucleus + 1  # adds a counter to the counter
+                    count_nucleus = -1  #counter
+                    nucleus_list = []  #list for all found nuclei
+                    for nucleus in position_nucleus:  #goes throw all nuclei positions
+                        nucleus_position = shapely.geometry.Point(nucleus) #creates a point at the center of the nucleus object
+                        count_nucleus = count_nucleus + 1  #ticks up the counter
 
-                        if nucleus_position.within(cell_shape) == True:  # checks if the center of the nucleus is within the cell
-                            nucleus_list.append(count_nucleus)
+                        if nucleus_position.within(cell_shape) == True:  #checks if the center of the nucleus is within the cell
+                            nucleus_list.append(count_nucleus) #appends the nucleus count to the list for all found nuclei
 
-                    if len(nucleus_list) == 1:  # checks the lenth of the nucleus list
+                    if len(nucleus_list) == 1:  #checks the length of the nucleus list
 
-                        new_row = {}  # Creates a new Row for all the localistions
+                        new_row = {}  #a new row for the pandas data frame
 
                         ImageNumber = DataFrameDict_Full_cell[key]['ImageNumber'].to_list()[count_cell]
                         Location_Center_X = DataFrameDict_Full_cell[key]['Location_Center_X'].to_list()[count_cell]
@@ -119,13 +122,14 @@ def cell_to_organell_basic(Cells, Cytoplasm, Nucleus):
                         AreaShape_MinFeretDiameter = DataFrameDict_Full_cell[key]['AreaShape_MinFeretDiameter'].to_list()[count_cell]
                         AreaShape_MaxFeretDiameter = DataFrameDict_Full_cell[key]['AreaShape_MaxFeretDiameter'].to_list()[count_cell]
 
+                        #adds relevant information to the new row
                         new_row.update({'ImageNumber': ImageNumber,
                                         'Location_Center_X': Location_Center_X,
                                         'Location_Center_Y': Location_Center_Y,
                                         'AreaShape_MinFeretDiameter': AreaShape_MinFeretDiameter,
                                         'AreaShape_MaxFeretDiameter': AreaShape_MaxFeretDiameter})
 
-                        # adds the mean intenitys to the new row
+                        #adds the mean intensities of the full cell and subcellular localizations to the new row
                         for marker in get_markers_from_segmentation(Cells):
                             MeanIntensity_Cell = DataFrameDict_Full_cell[key]['Intensity_' + marker].to_list()[count_cell]
                             MeanIntensity_Cytoplasm = DataFrameDict_Cytoplasm[key]['Intensity_' + marker].to_list()[count_cytoplasm]
@@ -135,7 +139,7 @@ def cell_to_organell_basic(Cells, Cytoplasm, Nucleus):
                                             marker + '_Cytoplasm': MeanIntensity_Cytoplasm,
                                             marker + '_Nucleus': MeanIntensity_Nucleus})
 
-                        single_nuclear_cells.loc[len(single_nuclear_cells)] = new_row  # adds the new row to the dataframe
+                        single_nuclear_cells.loc[len(single_nuclear_cells)] = new_row  #adds the new row to the pandas data frame
                     break
 
     return (single_nuclear_cells)
@@ -143,49 +147,48 @@ def cell_to_organell_basic(Cells, Cytoplasm, Nucleus):
 
 folder_dir = r'D:\ATF6'  #folder directory
 
-# Matching subcellular locations
-for paths, dirs, files in sd.walk(folder_dir):  # goes throw all files and folders in given directory
+for paths, dirs, files in sd.walk(folder_dir): #goes throw all files and folders in given directory
 
-    for file in os.listdir(paths):  # goes throw all files in a folder
-        filedir = os.path.join(paths, file)  # returns full file directory
+    for file in os.listdir(paths): #goes throw all files in a folder
+        filedir = os.path.join(paths, file)  #returns full file directory
 
-        if filedir.endswith(".csv"):  # returns all files that end with txt
+        if filedir.endswith(".csv"):  #returns all files that end with ".csv"
 
-            filename = os.path.basename(file)  # gives you the file name
-            filename_string = str(filename)
+            filename = os.path.basename(file) #gives you the file name
+            filename_string = str(filename) #turns the filename into a string
 
-            if 'RunCellpose_C' in filename_string:  # checks for a condition in the string
-                dir_list = []  # creates a list to hold the other directories
+            if 'RunCellpose_C' in filename_string: #checks if 'RunCellpose_C' is in the string
+                dir_list = []  #a list to hold the other directories
 
                 dir_list.append(filedir)
-                filedir_string = str(filedir)[:-len(filename) - 5]  # creates a file directory string
+                filedir_string = str(filedir)[:-len(filename) - 5]  #gives you the file directory as a string
 
-                filedir_images_string = filedir_string + r'\subcellular localisation'  # creates a new directory
+                filedir_images_string = filedir_string + r'\subcellular localisation'  #creates a new directory
 
-                if os.path.isdir(filedir_images_string) == True:  # checks if the new directory allready exists
+                if os.path.isdir(filedir_images_string) == True:  #checks if the new directory allready exists
                     pass
-                else:
-                    os.makedirs(filedir_images_string)  # creates the new directory
+                else: #creates a new directory if necessary
+                    os.makedirs(filedir_images_string)
 
-                for paths, dirs, files in sd.walk(
-                        filedir_string + '/csv'):  # goes throw all files and folders in given directory
+                for paths, dirs, files in sd.walk(filedir_string + '/csv'):  #goes throw all files and folders in given directory
 
-                    for file in os.listdir(paths):  # goes throw all files in a folder
-                        filedir = os.path.join(paths, file)
+                    for file in os.listdir(paths):  #goes throw all files in a folder
+                        filedir = os.path.join(paths, file) #returns full file directory
                         filedir_string = str(filedir)  # creates a file directory string
 
-                        if 'RunCellpose_N' in filedir_string:  # checks for a condition in the other files
+                        if 'RunCellpose_N' in filedir_string:  #checks if 'RunCellpose_N' is in the string
                             dir_list.append(filedir)
-                        if 'Cytoplasm' in filedir_string:  # checks for a condition in the other files
+                        if 'Cytoplasm' in filedir_string:  #checks if 'Cytoplasm' is in the string
                             dir_list.append(filedir)
 
-                        if len(dir_list) == 3:  # if three files are found the analisis is done
+                        if len(dir_list) == 3:  #if three files are found the matching is performed
 
                             print(dir_list)
 
-                            Full_cell = pd.read_csv(dir_list[0])  # reads the found files
+                            #reads in the found files
+                            Full_cell = pd.read_csv(dir_list[0])  
                             Cytoplasm = pd.read_csv(dir_list[1])
                             Nucleus = pd.read_csv(dir_list[2])
 
-                            single_cells_and_organells = cell_to_organell_basic(Full_cell, Cytoplasm, Nucleus,)  # matches subcellular locations
-                            single_cells_and_organells.to_csv(filedir_images_string + '/' + filename_string[:-18] + '_single_cells_and_organells.csv')  # exports the file
+                            single_cells_and_organells = cell_to_organell_basic(Full_cell, Cytoplasm, Nucleus,)  #links objects
+                            single_cells_and_organells.to_csv(filedir_images_string + '/' + filename_string[:-18] + '_single_cells_and_organells.csv') #exports the file
