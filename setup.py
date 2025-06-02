33a57f8757f2f6e22591a4adbeab2eb14d205564
Niklas Vesper <niklas.vesper@uniklinik-freiburg.de>
2025-06-02 17:02:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,16 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
-setup(
-    name="ImagingCytometryTools",
-    version="1.0.0",
-    description="Analysis of complex imaging data",
-    long_description=long_description,
-    url="https://github.com/NiklasVesper/ImagingCytometryTools",
-    author="Niklas Vesper",
-    author_email="niklas.vesper@uniklinik-freiburg.de",
-    packages=find_packages(where="ImagingCytometryTools"),
-    include_package_data=True,
-    license="LICENSE.txt",
-    install_requires=open("requirements.txt").read(),
-    classifiers=["Programming Language :: Python :: 3.8", "License :: MIT License", "Topic :: Scientific/Engineering :: Bio-Informatics", "Topic :: Scientific/Engineering :: Visualization",], 
-    python_requires=">=3.8"
-)
+setup(name="ImagingCytometryTools",
+      version="1.0.0",
+      description="Analysis of complex imaging data",
+      long_description=long_description,
+      url="https://github.com/NiklasVesper/ImagingCytometryTools",
+      author="Niklas Vesper",
+      author_email="niklas.vesper@uniklinik-freiburg.de",
+      packages=find_packages(where="ImagingCytometryTools"),
+      include_package_data=True,
+      license="LICENSE.txt",
+      install_requires=open("requirements.txt").read(),
+      classifiers=["Programming Language :: Python :: 3.8", "License :: MIT License", "Topic :: Scientific/Engineering :: Bio-Informatics", "Topic :: Scientific/Engineering :: Visualization",], 
+      python_requires=">=3.8")
