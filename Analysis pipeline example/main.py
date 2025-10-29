eb1949bb6adfe08368208abab06074c4a507ddc1
Niklas Vesper <niklas.vesper@uniklinik-freiburg.de>
2025-10-29 14:00:42 +0100

    Update main.py

--- Analysis pipeline example/main.py
+++ Analysis pipeline example/main.py
@@ -1,1 +1,26 @@
+if __name__ == "__main__":
+  
+# Pre-processing-----------------------------------------------------------------------------------------------------------
+    folder_directory_with_spill_mat = r'C:\Users\vesper\Desktop\Spillovermatrix_IMC_2.txt'
+    folder_directory_with_data = r'D:\ATF6 FAK 2_0\Round 1'
 
+    # Compensation---------------------------------------------------------------------------------------------------------
+    from ImagingCytometryTools.run_compensation_IMC import run_compensation_IMC
+
+
+    run_compensation_IMC(folder_directory_with_data,
+                         folder_directory_with_spill_mat,
+                         use_GPU=True)
+
+
+    # Image and folder generation------------------------------------------------------------------------------------------
+    from ImagingCytometryTools.generate_images_from_IMC_data import generate_images_from_IMC_data
+    from ImagingCytometryTools.generate_folders_for_CellProfiler import generate_folders_for_CellProfiler
+
+
+    generate_images_from_IMC_data(folder_directory_with_data, 
+                                  generate_ome_tiff=False,
+                                  change_name_for_MCD_viewer=False)
+    
+    generate_folders_for_CellProfiler(folder_directory_with_data,
+                                      generate_folders_per_mcd_file=True)
